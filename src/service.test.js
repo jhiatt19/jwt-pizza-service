f147fdeec1fcf997c82a6ff8422c39d6d3c13e69
jhiatt19 <hiattj19@gmail.com>
2026-02-03 21:05:10 -0700

    This should now pass the full pipeline

--- src/service.test.js
+++ src/service.test.js
@@ -1,20 +1,24 @@
-import request from 'supertest';
-import app from './service';
+const request = require("supertest");
+const app = require("./service");
 
-const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
 
 beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
-  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
+  const registerRes = await request(app).post("/api/auth").send(testUser);
   testUserAuthToken = registerRes.body.token;
+  console.log(testUserAuthToken);
 });
 
-test('login', async () => {
-  const loginRes = await request(app).put('/api/auth').send(testUser);
+test("login", async () => {
+  const loginRes = await request(app).put("/api/auth").send(testUser);
   expect(loginRes.status).toBe(200);
-  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+  expect(loginRes.body.token).toMatch(
+    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
+  );
 
-  const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
   expect(loginRes.body.user).toMatchObject(user);
+  console.log(password);
 });
