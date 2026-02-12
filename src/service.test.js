b1cbdcc49ecc748ff2accdb112c2e0ddc69220ea
jhiatt19 <hiattj19@gmail.com>
2026-02-11 21:04:52 -0700

    Mocking attempt, committing in case I want to put it back in

--- src/service.test.js
+++ src/service.test.js
@@ -1,110 +1,134 @@
+const express = require("express");
+
+jest.mock("./routes/authRouter.js", () => {
+  const actualExpress = jest.requireActual("express");
+  const mockRouter = actualExpress.Router();
+  return {
+    authRouter: mockRouter,
+    setAuthUser: jest.fn(),
+    setAuth: jest.fn(),
+  };
+});
+
+jest.mock("./database/database.js", () => {
+  return {
+    DB: jest.fn().mockImplementation(() => {
+      return {
+        getMenu: jest.fn(),
+        initializeDatabase: jest.fn().mockResolvedValue(true),
+      };
+    }),
+  };
+});
+
+const { DB } = require("./database/database.js");
 const request = require("supertest");
 const app = require("./service");
-const { DB } = require("./database/database.js");
-const { Role } = require("./model/model.js");
+const authRouter = require("./routes/authRouter.js");
+const userRouter = require("./routes/userRouter.js");
 
-const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
-const registerAdmin = () => createAdminUser();
-const newMenu = {
-  title: "Student",
-  description: "No topping, no sauce, just carbs",
-  image: "pizza9.png",
-  price: 0.0001,
-};
-
-const randomname = () => `User_${Math.random().toString(36).substring(2, 9)}`;
-
-async function createAdminUser() {
-  let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
-  user.name = randomname();
-  user.email = user.name + "@admin.com";
-
-  await DB.addUser(user);
-  user.password = "toomanysecrets";
-
-  return user;
-}
-
-let testUserAuthToken;
-let adminUserAuthToken;
-
-beforeAll(async () => {
-  testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
-  const registerRes = await request(app)
-    .post("/api/auth")
-    .send({ ...testUser, roles: { role: "diner" } });
-  testUserAuthToken = registerRes.body.token;
-
-  adminUser = await registerAdmin();
-});
-
-test("login", async () => {
-  const loginRes = await request(app).put("/api/auth").send(testUser);
-  expect(loginRes.status).toBe(200);
-  expect(loginRes.body.token).toMatch(
-    /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
-  );
-
-  const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
-  expect(loginRes.body.user).toMatchObject(user);
-  console.log(loginRes.body);
-
-  const loginAdmin = await request(app).put("/api/auth").send(adminUser);
-  adminUserAuthToken = loginAdmin.body.token;
-});
-
-test("get menu", async () => {
-  const menuResult = await request(app).get("/api/order/menu");
-  expect(menuResult.status).toBe(200);
-
-  const expected = [
-    {
+test("GET /me", async () => {
+  authRouter.authenticateToken.mockImplementation((req, res, next) => {
+    req.user = {
       id: 1,
-      title: "Veggie",
-      image: "pizza1.png",
-      price: 0.0038,
-      description: "A garden of delight",
-    },
+      name: "bill",
+      email: "a@jwt.com",
+      roles: [{ role: "admin" }],
+    };
+    next();
+  });
+
+  const response = await request(app).get("/api/user/me");
+
+  expect(response.statusCode).toBe(200);
+  expect(response.body.name).toBe("bill");
+});
+
+test("Get Menu", async () => {
+  const fakeMenu = [
     {
-      id: 2,
-      title: "Pepperoni",
-      image: "pizza2.png",
-      price: 0.0042,
-      description: "Spicy treat",
-    },
-    {
-      id: 3,
-      title: "Margarita",
-      image: "pizza3.png",
-      price: 0.0042,
-      description: "Essential classic",
-    },
-    {
-      id: 4,
-      title: "Crusty",
-      image: "pizza4.png",
-      price: 0.0028,
-      description: "A dry mouthed favorite",
-    },
-    {
-      id: 5,
-      title: "Charred Leopard",
-      image: "pizza5.png",
-      price: 0.0099,
-      description: "For those with a darker side",
+      title: "Student",
+      description: "No topping, no sauce, just carbs",
+      image: "pizza9.png",
+      price: 0.0001,
     },
   ];
-  expect(menuResult.body).toMatchObject(expected);
+  const myDb = new DB();
+  myDb.getMenu.mockResolvedValue(fakeMenu);
+
+  const result = await myDb.getMenu();
+
+  expect(result).toEqual(fakeMenu);
+  expect(myDb.getMenu).toHaveBeenCalled();
 });
 
-test("add menu", async () => {
-  const addMenuRes = await request(app)
-    .put("/api/order/menu")
-    .set("Authorization", `Bearer ${adminUserAuthToken}`)
-    .send(newMenu);
+// test("login", async () => {
+//   const loginRes = await request(app).put("/api/auth").send(testUser);
+//   expect(loginRes.status).toBe(200);
+//   expect(loginRes.body.token).toMatch(
+//     /^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/,
+//   );
 
-  expect(addMenuRes.status).toBe(200);
+//   const { password, ...user } = { ...testUser, roles: [{ role: "diner" }] };
+//   expect(loginRes.body.user).toMatchObject(user);
+//   console.log(loginRes.body);
 
-  console.log(addMenuRes.body.length);
-  expect(addMenuRes.body.toString()).toMatch(newMenu.toString());
-});
+//   const loginAdmin = await request(app).put("/api/auth").send(adminUser);
+//   adminUserAuthToken = loginAdmin.body.token;
+// });
+
+// test("get menu", async () => {
+//   const menuResult = await request(app).get("/api/order/menu");
+//   expect(menuResult.status).toBe(200);
+
+//   const expected = [
+//     {
+//       id: 1,
+//       title: "Veggie",
+//       image: "pizza1.png",
+//       price: 0.0038,
+//       description: "A garden of delight",
+//     },
+//     {
+//       id: 2,
+//       title: "Pepperoni",
+//       image: "pizza2.png",
+//       price: 0.0042,
+//       description: "Spicy treat",
+//     },
+//     {
+//       id: 3,
+//       title: "Margarita",
+//       image: "pizza3.png",
+//       price: 0.0042,
+//       description: "Essential classic",
+//     },
+//     {
+//       id: 4,
+//       title: "Crusty",
+//       image: "pizza4.png",
+//       price: 0.0028,
+//       description: "A dry mouthed favorite",
+//     },
+//     {
+//       id: 5,
+//       title: "Charred Leopard",
+//       image: "pizza5.png",
+//       price: 0.0099,
+//       description: "For those with a darker side",
+//     },
+//   ];
+//   expect(menuResult.body).toMatchObject(expected);
+// });
+
+// test("add menu", async () => {
+//   const addMenuRes = await request(app)
+//     .put("/api/order/menu")
+//     .set("Authorization", `Bearer ${adminUserAuthToken}`)
+//     .send(newMenu);
+
+//   expect(addMenuRes.status).toBe(200);
+
+//   console.log(addMenuRes.body.length);
+//   expect(addMenuRes.body.toString()).toMatch(newMenu.toString());
+// });
