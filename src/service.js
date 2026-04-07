7a7006356533fde3e4e1f34677a73ff61adac311
jhiatt19 <hiattj19@gmail.com>
2026-04-07 03:22:53 -0600

    Go back to previous metric finder

--- src/service.js
+++ src/service.js
@@ -10,11 +10,12 @@ const dataBaseFixingRouter = require("./routes/databaseFixingRouter.js");
 const logger = require("./logging.js");
 
 const app = express();
-const start = Date.now();
+
 app.use(logger.httpLogger);
 app.use(express.json());
 app.use(setAuthUser);
 app.use((req, res, next) => {
+  const start = Date.now();
   res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
