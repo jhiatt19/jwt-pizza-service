0797aabcd3e0004094825423336796b1a3206612
jhiatt19 <hiattj19@gmail.com>
2026-04-10 15:56:53 -0600

    Closed my database reset

--- src/routes/databaseFixingRouter.js
+++ src/routes/databaseFixingRouter.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const { asyncHandler } = require("../endpointHelper");
-const { DB } = require("../database/database.js");
+// const express = require("express");
+// const { asyncHandler } = require("../endpointHelper");
+// const { DB } = require("../database/database.js");
+// const { authRouter } = require("./authRouter.js");
 
-const dataBaseFixingRouter = express.Router();
+// const dataBaseFixingRouter = express.Router();
 
-dataBaseFixingRouter.post(
-  "/reset",
-  asyncHandler(async () => {
-    const conn = await DB.getConnection();
-    DB.resetDatabase(conn);
-  }),
-);
+// dataBaseFixingRouter.post(
+//   "/reset",
+//   authRouter.authenticateToken,
+//   asyncHandler(async () => {
+//     const conn = await DB.getConnection();
+//     DB.resetDatabase(conn);
+//   }),
+// );
 
-module.exports = dataBaseFixingRouter;
+// module.exports = dataBaseFixingRouter;
